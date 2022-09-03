5cd10bafe2ad0ca4c49cd8c8cdb39d3b60575d47
Lalit Bodana <lalitboda@gmail.com>
2022-09-03 12:26:11 +0530

    checking dev commit is working or not

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { AboutComponent } from './core/components/about/about.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
